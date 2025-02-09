a9aab072498d491e2e1ea58bbf083c559065b4a0
Fabio-Frare <44546034+Fabio-Frare@users.noreply.github.com>
2025-02-09 20:49:38 -0300

    Implementado tweets de outros usuários na timeline

--- App/Models/Tweet.php
+++ App/Models/Tweet.php
@@ -35,7 +35,11 @@ class Tweet extends Model {
             t.id, t.id_usuario, u.nome, t.tweet, DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') as data 
         from tweets as t
             left join usuarios as u on(t.id_usuario = u.id)
-        where id_usuario = :id_usuario
+        where 
+            id_usuario = :id_usuario or
+            t.id_usuario in(
+                select id_usuario_seguindo from usuarios_seguidores
+                where id_usuario = :id_usuario)
         order by t.data desc";
 
         $stmt = $this->db->prepare($query);
